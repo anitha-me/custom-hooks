3c0a771b59afad542ffc9cc6b2d9f312c925890d
Anithabtc <anitha.me@bigthinkcode.com>
2025-02-20 12:39:42 +0530

    updated use auth

--- src/hooks/useAuth.ts
+++ src/hooks/useAuth.ts
@@ -1,12 +1,4 @@
-import { useEffect, useState } from "react";
-import toast from "react-hot-toast";
-import { v4 as uuidv4 } from "uuid";
-
-import { getLocalStorageValue, setLocalStorageValue } from "./useLocalStorage";
-import { Errors } from "../constants/errors";
-import { useApiRequest } from "../lib/axios/useApiRequest";
-import { getAuthorizationUrl } from "../repositories/login.repositories";
-import { jwtDecode } from "../utils/login.utils";
+import { useEffect, useState } from 'react';
 
 interface Props {
   setIsLoading?: (loading: boolean) => void;
@@ -20,56 +12,28 @@ interface Props {
  */
 export const useAuth = ({
   setIsLoading = () => {},
-  scope: authScope = "",
+  scope = '', // can also authorize specific scopes (add them in url)
   successCallback,
 }: Props) => {
-  const userData = getLocalStorageValue("user_data");
   const [isSignIn, setIsSignIn] = useState<boolean>(false);
 
-  const { callApi: api_getAuthorizationUrl } = useApiRequest(
-    getAuthorizationUrl,
-    false
-  );
-
-  // Handle the Oauth event listener.
   useEffect(() => {
     const handleMessage = async (event: MessageEvent) => {
-      if (
-        event.origin == process.env.VITE_ORIGIN &&
-        event.data.includes("id_token")
-      ) {
-        setIsLoading(true);
+      try {
+        // TODO: Handle sign event data
+        console.log(event);
 
-        const {
-          id_token: idToken,
-          app_oauth_id: appOauthId,
-          access_token: accessToken,
-        } = JSON.parse(event.data);
-        const data = jwtDecode(idToken);
-
-        try {
-          const sessionId = uuidv4();
-
-          setLocalStorageValue("user_data", {
-            ...data,
-            idToken,
-            sessionId,
-            appOauthId,
-            accessToken,
-          });
-
-          successCallback && (await successCallback());
-        } catch (err) {
-          toast.error(Errors.Authorize);
-        } finally {
-          setIsLoading(false);
-        }
+        successCallback && (await successCallback());
+      } catch (err) {
+        console.error(err);
+      } finally {
+        setIsLoading(false);
       }
     };
 
-    window.addEventListener("message", handleMessage);
+    window.addEventListener('message', handleMessage);
     return () => {
-      window.removeEventListener("message", handleMessage);
+      window.removeEventListener('message', handleMessage);
     };
   }, [successCallback]);
 
@@ -79,20 +43,20 @@ export const useAuth = ({
   const initOauth = async () => {
     setIsLoading(true);
     try {
-      const res = await api_getAuthorizationUrl("google", authScope);
+      const res = await fetch('/');
       window.open(res.url);
     } catch (err) {
-      toast.error(Errors.Authorize);
+      console.error(err);
     } finally {
       setIsLoading(false);
     }
   };
 
   useEffect(() => {
-    if (userData?.idToken) {
-      setIsSignIn(true);
-    }
-  }, [userData]);
+    // TODO: Condition to check if user is logged in or not
+    // and update setIsSignIn state
+    setIsSignIn(true);
+  }, []);
 
   return { isSignIn, initOauth };
 };
